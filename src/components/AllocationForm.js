492739820c7ba6a7dbea870edb875022a0a38f1f
Stephanie Haber <stephanie.haber@outlook.de>
2024-01-24 11:44:24 -0500

    changes made from theia lab environment

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const AllocationForm = (props) => {
-    const { dispatch,remaining  } = useContext(AppContext);
+    const { dispatch,remaining, currency  } = useContext(AppContext);
 
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
@@ -59,7 +59,7 @@ const AllocationForm = (props) => {
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
 
-                    $<input
+                  {currency} <input
                         required='required'
                         type='number'
                         id='cost'
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,14 +2,14 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, currency } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
     }
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input></span>
+<span>Budget: {currency} <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input></span>
 </div>
     );
 };
